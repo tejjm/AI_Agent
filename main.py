00dbefe0a7769f90794a94f5f4306f87fe464bc7
tejjm <uttej.madhusudhan@gmail.com>
2026-02-09 11:30:37 +0000

    Token count added

--- main.py
+++ main.py
@@ -19,4 +19,9 @@ response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents = r"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
+if response.usage_metadata is None:
+    raise RuntimeError("Error homie")
+else:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
