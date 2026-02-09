b00b0c8d55f2464253becba12b1ea86053f4604d
tejjm <uttej.madhusudhan@gmail.com>
2026-02-09 09:32:21 +0000

    Initializing the model and contents

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ if __name__ == "__main__":
     main()
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
-print(api_key)
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found in environment")
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents = r"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
