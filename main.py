5ffff636b668764e3c317715f9a9955024060644
tejjm <uttej.madhusudhan@gmail.com>
2026-02-10 15:03:57 +0000

    user prompt

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
@@ -16,13 +17,20 @@ if api_key is None:
     raise RuntimeError("GEMINI_API_KEY not found in environment")
 
 client = genai.Client(api_key=api_key)
+
+# Adding argparse to take user prompt
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
+
+#Saving the messages to a list 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents = args.user_prompt
+    contents = messages
 )
+
 if response.usage_metadata is None:
     raise RuntimeError("Error homie")
 else:
