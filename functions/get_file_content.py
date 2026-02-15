d78de0cf14d4b98757f1fc1957797ff6c0ff70e5
tejjm <uttej.madhusudhan@gmail.com>
2026-02-15 02:15:47 +0000

    File content starting file done

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,8 +1,20 @@
 import os
+from config import MAX_CHAR
 def get_file_content(working_directory, file_path):
-    abs_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(os.path.join(working_directory,file_path))
-    common_path = os.path.commonpath(abs_dir,abs_file_path)
-    if not common_path == abs_dir:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory' 
+    try:
+        abs_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.abspath(os.path.join(working_directory,file_path))
+        common_path = os.path.commonpath(abs_dir,abs_file_path)
+        if not common_path == abs_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory' 
+        if not os.path.isfile(file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        with open(abs_file_path,"r") as f:
+            content = f.read(1000)
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at 10000 characters]'
+
     
+    except Exception as e:
+        return f'Error: {e}'
+
