1269dc11cc83ecd0ad843b2a9ed95279a15b65dc
tejjm <uttej.madhusudhan@gmail.com>
2026-02-16 15:21:05 +0000

    Test files successfully cleared for writing function

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,8 +2,8 @@ import os
 def write_file(working_directory, file_path, content):
     try:
         abs_work = os.path.abspath(working_directory)
-        abs_file_path = os.path.join(working_directory,os.path.abspath(file_path))
-        if not os.path.commonpath([abs_work,abs_file_path]) == abs_file_path:
+        abs_file_path = os.path.abspath(os.path.join(abs_work, file_path))
+        if os.path.commonpath([abs_work,abs_file_path]) != abs_work:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
         if os.path.isdir(abs_file_path):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
@@ -13,5 +13,5 @@ def write_file(working_directory, file_path, content):
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
-        return f"Error:{e}"
+        return f"Error: {e}"
     
