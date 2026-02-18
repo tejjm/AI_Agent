85de117d0617edea84226c4b6f246c22578e9af7
tejjm <uttej.madhusudhan@gmail.com>
2026-02-18 17:23:19 +0000

    python running function code done

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 def run_python_file(working_directory, file_path, args=None):
         try:
             abs_work = os.path.abspath(working_directory)
@@ -10,6 +11,46 @@ def run_python_file(working_directory, file_path, args=None):
             if not abs_file_path.endswith('.py'):
                 return f'Error: "{file_path}" is not a Python file'
             command = ["python", abs_file_path]
+            if args:
+                command.extend(args)
+            result = subprocess.run(
+                 command,
+                cwd=abs_work,       
+                capture_output=True, 
+                text=True,            
+                timeout=30)
+            output = []
+            if result.returncode != 0:
+                 output.append(f"Process exited with code {result.returncode}")
+            if result.stderr:
+                 output.append(f" STDERR: {result.stderr}")
+            if result.stdout:
+                output.append(f" STDOUT: {result.stdout}")
+            elif not result.stderr and not result.stdout:
+                 output.append(" No output produced")
+            return "\n".join(output)
+        except Exception as e:
+             return f"Error: executing Python file: {e}"
+    
+
+                 
+            
+
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         except Exception as e:
              return f"Error: executing Python file: {e}"
