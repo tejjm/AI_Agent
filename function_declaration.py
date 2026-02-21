58feca83c414728d20b0faf5760f899137dcd960
tejjm <uttej.madhusudhan@gmail.com>
2026-02-21 05:54:57 +0000

    Added write_file function declaration

--- function_declaration.py
+++ function_declaration.py
@@ -29,12 +29,45 @@ schema_get_file_content = types.FunctionDeclaration(
     )
 )
 
-schema_run_python = types.FunctionDeclaration(
-
-
-)
-
 schema_write_file = types.FunctionDeclaration(
-    
-
+    name="write_file",
+    description="Writes the given content to the file located at given file path",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path":types.Schema(
+                type=types.Type.STRING,
+                description="File path for file on which writing operation needed to be done"
+            ),
+            "content":types.Schema(
+                type=types.Type.STRING,
+                description="Content which needs to be written to the given file path"
+            )},
+        required=["file_path","content"]
+        
+    )
 )
+
+schema_run_python_file = types.FunctionDeclaration(
+    name = "run_python_file",
+    description="Runs a python file and returns a output/error list",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path":types.Schema(
+            type=types.Type.STRING,
+            description="This is the path given to file which we are looking to find"
+            ),
+            "args":types.Schema(
+                type=types.Type.ARRAY,
+                items = types.Schema(type=types.Type.STRING),
+                description="Arguments which are passed down to the python file"
+            )
+            },
+        required=["file_path"]
+        )
+    )
+
+
+
+
