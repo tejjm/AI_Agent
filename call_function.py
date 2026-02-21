50ad366cf8101d522f03e28256f46accdf7da206
tejjm <uttej.madhusudhan@gmail.com>
2026-02-21 05:57:46 +0000

    Updated prompt

--- call_function.py
+++ call_function.py
@@ -3,7 +3,7 @@ import function_declaration as fd
 
 available_functions = types.Tool(
     function_declarations=[fd.schema_get_files_info,
-                           fd.schema_get_files_content,
-                           fd.schema_run_python,
+                           fd.schema_get_file_content,
+                           fd.schema_run_python_file,
                            fd.schema_write_file],
 )
